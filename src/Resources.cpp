fbfc3902d676e8c6d6aafaa2c88dfcc498336b07
DarkContact <DarkContact@mail.ru>
2025-08-29 00:37:32 +0500

    std::format

--- src/Resources.cpp
+++ src/Resources.cpp
@@ -1,7 +1,7 @@
 #include "Resources.h"
 
 #include <filesystem>
-#include <iostream>
+#include <format>
 
 Resources::Resources(std::string_view rootDirectory) :
     m_rootDirectory(rootDirectory)
@@ -12,15 +12,19 @@ std::vector<std::string> Resources::levelNames() const
     std::vector<std::string> results;
 
     try {
-        std::string path = std::string(m_rootDirectory) + std::string("/levels/single");
-        for (const auto& entry : std::filesystem::directory_iterator(path)) {
+        for (const auto& entry : std::filesystem::directory_iterator(std::format("{}/levels/single", m_rootDirectory))) {
             if (entry.is_directory()) {
                 results.push_back(entry.path().filename().string());
             }
         }
     } catch (const std::filesystem::filesystem_error& e) {
-        std::cerr << "Filesystem error: " << e.what() << '\n';
+        fprintf(stderr, "Filesystem error: %s", e.what());
     }
 
     return results;
+}
+
+std::string Resources::levelSef(std::string_view level, std::string_view levelType) const
+{
+    return std::format("{0}/levels/{1}/{2}/{2}.sef", m_rootDirectory, levelType, level);
 }
