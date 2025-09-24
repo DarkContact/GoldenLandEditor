0abd1541dc02905804e66d6a3d4e2bef22e8ecac
DarkContact <DarkContact@mail.ru>
2025-09-24 10:45:50 +0500

    Отображение CellGroups через линии, а не прямоугольники

--- src/windows/LevelViewer.cpp
+++ src/windows/LevelViewer.cpp
@@ -594,28 +594,25 @@ void LevelViewer::drawPointsEntrance(Level& level, ImVec2 drawPosition)
 void LevelViewer::drawCellGroups(Level& level, ImVec2 drawPosition)
 {
     ImDrawList* drawList = ImGui::GetWindowDrawList();
+    auto drawListFlags = drawList->Flags;
+    drawList->Flags = ImDrawListFlags_None;
+
     for (const SEF_CellGroup& group : level.data().sefData.cellGroups) {
         if (group.cells.empty()) break;
 
-        const TilePosition* leftCell = &group.cells.front();
-        const TilePosition* rightCell = &group.cells.front();
-        const TilePosition* upCell = &group.cells.front();
-        const TilePosition* downCell = &group.cells.front();
-
+        std::vector<ImVec2> centers;
         for (int i = 0; i < group.cells.size(); ++i) {
             const TilePosition& cellPosition = group.cells[i];
 
-            if (cellPosition.x < leftCell->x) {
-                leftCell = &cellPosition;
-            } else if (cellPosition.x > rightCell->x) {
-                rightCell = &cellPosition;
-            }
+            ImVec2 position(drawPosition.x + cellPosition.x * Level::tileWidth,
+                            drawPosition.y + cellPosition.y * Level::tileHeight);
 
-            if (cellPosition.y < upCell->y) {
-                upCell = &cellPosition;
-            } else if (cellPosition.y > downCell->y) {
-                downCell = &cellPosition;
-            }
+            centers.push_back({position.x + Level::tileWidth / 2, position.y + Level::tileHeight / 2});
+        }
+        drawList->AddPolyline(&centers[0], centers.size(), IM_COL32(0, 0, 0, 180), 0, 2.0f);
+
+        for (int i = 0; i < group.cells.size(); ++i) {
+            const TilePosition& cellPosition = group.cells[i];
 
             ImVec2 position(drawPosition.x + cellPosition.x * Level::tileWidth,
                             drawPosition.y + cellPosition.y * Level::tileHeight);
@@ -644,13 +641,9 @@ void LevelViewer::drawCellGroups(Level& level, ImVec2 drawPosition)
             drawList->AddRectFilled(position, {position.x + Level::tileWidth, position.y + Level::tileHeight}, IM_COL32(51, 255, 204, fullAlpha ? 192 : 64));
             drawList->AddRect(position, {position.x + Level::tileWidth, position.y + Level::tileHeight}, IM_COL32(0, 0, 0, fullAlpha ? 192 : 64));
         }
-
-        ImVec2 boxUpLeft(drawPosition.x + leftCell->x * Level::tileWidth,
-                          drawPosition.y + upCell->y * Level::tileHeight);
-        ImVec2 boxDownRight(drawPosition.x + rightCell->x * Level::tileWidth + Level::tileWidth,
-                            drawPosition.y + downCell->y * Level::tileHeight + Level::tileHeight);
-        drawList->AddRect(boxUpLeft, boxDownRight, IM_COL32(0, 0, 0, 220), 0.0f, 0, 2.0f);
     }
+
+    drawList->Flags = drawListFlags;
 }
 
 std::string LevelViewer::personInfo(const SEF_Person& person)
