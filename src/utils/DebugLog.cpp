08a23713389c028595da3fef6770a60b6a4a5931
DarkContact <DarkContact@mail.ru>
2025-10-05 14:59:00 +0500

    Доработка класса DebugLog

--- src/utils/DebugLog.cpp
+++ src/utils/DebugLog.cpp
@@ -1,1 +1,18 @@
 #include "DebugLog.h"
+
+#include <cassert>
+
+#include "TracyProfiler.h"
+
+void DebugLog::toOutput(const std::source_location& location, std::string_view msg) {
+    Tracy_Message(msg.data(), msg.size());
+
+    fputs(msg.data(), stdout);
+
+    constexpr size_t kBufferSize = 512;
+    char bufferInfo[kBufferSize];
+    auto result = std::format_to_n(bufferInfo, kBufferSize, " [{}:{}]\n", location.file_name(), location.line());
+    assert(result.size < kBufferSize);
+    bufferInfo[result.size] = '\0';
+    fputs(bufferInfo, stdout);
+}
