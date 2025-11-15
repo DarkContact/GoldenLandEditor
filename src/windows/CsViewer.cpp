a076e55cb0335fc7b4aef7bb18d9ce8ad72c5b49
DarkContact <DarkContact@mail.ru>
2025-11-16 01:16:42 +0500

    Отображение строк из диалогов в CsViewer

--- src/windows/CsViewer.cpp
+++ src/windows/CsViewer.cpp
@@ -7,14 +7,25 @@
 #include "utils/DebugLog.h"
 #include "utils/StringUtils.h"
 #include "utils/TracyProfiler.h"
-#include "parsers/CS_Parser.h"
 
-static std::string csNodeString(const CS_Node& node) {
+#include "parsers/CS_Parser.h"
+#include "parsers/SDB_Parser.h"
+
+static std::string csNodeString(const CS_Node& node, const SDB_Data& sdbDialogs) {
     std::string additionInfo;
     if (node.opcode >= 0 && node.opcode <= 20) {
         additionInfo = std::format("a: {}, b: {}, c: {}, d: {}", node.a, node.b, node.c, node.d);
     } else if (node.opcode == 21 || node.opcode == 24) {
-        additionInfo = std::format("val: {}", node.value);
+        if (node.opcode == 24) {
+            auto it = sdbDialogs.strings.find(node.value);
+            if (it != sdbDialogs.strings.cend()) {
+                additionInfo = std::format("val: {} [{}]", node.value, it->second);
+            } else {
+                additionInfo = std::format("val: {}", node.value);
+            }
+        } else {
+            additionInfo = std::format("val: {}", node.value);
+        }
     } else if (node.opcode == 22 || node.opcode == 23) {
         additionInfo = std::format("txt: {}", node.text);
     } else if (node.opcode == 48) {
@@ -42,9 +53,19 @@ void CsViewer::update(bool& showWindow, std::string_view rootDirectory, const st
     static ImGuiTextFilter textFilterFile;
     static ImGuiTextFilter textFilterString;
     static CS_Data csData;
+    static SDB_Data sdbDialogs;
     static std::string csError;
+    static bool onceWhenOpen = false;
 
     bool needResetScroll = false;
+
+    if (!onceWhenOpen) {
+        std::string error;
+        std::string sdbPath = std::format("{}/sdb/dialogs/dialogsphrases.sdb", rootDirectory);
+        if (!SDB_Parser::parse(sdbPath, sdbDialogs, &error))
+            LogFmt("Load dialogsphrases.sdb error: {}", error);
+        onceWhenOpen = true;
+    }
 
     ImGui::SetNextWindowSize(ImGui::GetMainViewport()->WorkSize, ImGuiCond_FirstUseEver);
     ImGui::Begin("CS Viewer", &showWindow);
@@ -72,7 +93,7 @@ void CsViewer::update(bool& showWindow, std::string_view rootDirectory, const st
                     // std::string csDataInfo;
                     // int counter = 0;
                     // for (const auto& node : csData.nodes) {
-                    //     std::string nodeInfo = csNodeString(node);
+                    //     std::string nodeInfo = csNodeString(node, {});
                     //     csDataInfo += std::format("[i:{}] {}\n", counter, nodeInfo);
                     //     ++counter;
                     // }
@@ -104,7 +125,7 @@ void CsViewer::update(bool& showWindow, std::string_view rootDirectory, const st
                 int counter = 0;
                 ImGui::PushFont(NULL, 15.0f);
                 for (const auto& node : csData.nodes) {
-                    std::string nodeInfo = csNodeString(node);
+                    std::string nodeInfo = csNodeString(node, sdbDialogs);
                     if (textFilterString.PassFilter(nodeInfo.c_str())) {
                         ImGui::Text("[i:%d] %s", counter, nodeInfo.c_str());
                     }
@@ -126,5 +147,7 @@ void CsViewer::update(bool& showWindow, std::string_view rootDirectory, const st
         csError.clear();
         textFilterFile.Clear();
         textFilterString.Clear();
+        sdbDialogs = {};
+        onceWhenOpen = false;
     }
 }
